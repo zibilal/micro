f5c45dbb0205dbcc378b6cf379a6ecb88134e0b2
maps90 <dimas.abdulaziiz@mataharimall.com>
2017-01-13 23:20:25 +0700

    [WIP] configuration package

--- micro-api.go
+++ micro-api.go
@@ -1,35 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"github.com/labstack/gommon/color"
-	config "github.com/spf13/viper"
+	"github.com/mataharimall/micro-api/config"
 	"log"
 	"os"
 	"runtime"
 )
 
 const (
-	BASE_PATH = "$GOPATH/src/github.com/mataharimall/micro-api"
-	APP_VER   = "v1"
+	APP_VER = "v1"
 )
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	if err := InitConfig(); err != nil {
+	if err := config.Init(); err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-}
-
-func InitConfig() (err error) {
-	config.SetConfigName("app")
-	config.AddConfigPath(BASE_PATH + "/config")
-	if err := config.ReadInConfig(); err != nil {
-		log.Println(err)
-		return fmt.Errorf("%s: %s", color.Red("ERROR"), color.Yellow("config files not found."))
-	}
-	return
 }
 
 func main() {
