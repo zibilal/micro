ebe79bc2e9b096ff2c913da42b72bcb4b7e084c4
zibilal <bmuhamm@gmail.com>
2017-01-18 19:18:02 +0700

    Implement AS-276

---
+++ apps/loket/handlers/invoiceCreate.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"github.com/mataharimall/micro/container"
+	"github.com/mataharimall/micro/api"
+	"encoding/json"
+	"fmt"
+)
+
+type CreateInvoiceRequestResponse struct {
+	Request CreateInvoiceRequest
+	Response interface{}
+}
+
+type CreateInvoiceRequest struct {
+	Data Data `json:"data"`
+}
+
+type Data struct {
+	Tickets        []Ticket `json:"tickets"`
+	Attendee       Attendee `json:"attendee"`
+	OrderId        string   `json:"order_id"`
+	ExpirationType string   `json:"expiration_type"`
+	Notes          string   `json:"notes"`
+}
+
+type Ticket struct {
+	IdTicket string `json:"id_ticket"`
+	Quantity string `json:"qty"`
+}
+
+type Attendee struct {
+	FirstName  string `json:"firstname"`
+	LastName   string `json:"lastname"`
+	IdentityId string `json:"identity_id"`
+	Dob        string `json:"dob"`
+	Gender     string `json:"gender"`
+	Email      string `json:"email"`
+	Telephone  string `json:"telephone"`
+}
+
+func CreateInvoice(c echo.Context) error {
+	r := CreateInvoiceRequestResponse{}
+
+	if err := c.Bind(&r.Request); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	loket, ok := container.Get("api.loket").(*api.Loket)
+
+	fmt.Printf("Type %T\n", loket)
+	fmt.Printf("Value %v\n", loket)
+	if !ok {
+		fmt.Println("Internal server error")
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	fmt.Printf("Request %v\n", r.Request)
+
+	jbyte, err := json.Marshal(r.Request)
+	str := string(jbyte)
+	fmt.Println("String", str)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	loket.GetAuth().Post("/v3/invoice/create", "json", str)
+
+	return c.JSON(http.StatusOK, loket.Body)
+}
