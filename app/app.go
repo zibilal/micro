78745d6048c652bf5b66812b2d20a5bb3f067362
maps90 <dimas.abdulaziiz@mataharimall.com>
2017-01-19 16:04:34 +0700

    update glide lock, add mysql & redis dependencies

--- app/app.go
+++ app/app.go
@@ -1,9 +1,11 @@
 package app
 
 import (
-	"fmt"
 	"github.com/maps90/librarian"
+	d "github.com/maps90/librarian/datasource"
+	"github.com/maps90/librarian/cache"
 	"github.com/mataharimall/micro/api"
+	c "github.com/spf13/viper"
 )
 
 const BASE_PATH = "$GOPATH/src/github.com/mataharimall/micro"
@@ -12,7 +14,6 @@ func Construct() (err error) {
 	err = initConfig(BASE_PATH)
 	err = InitApp()
 	err = initRouter()
-	fmt.Print("a")
 	return
 }
 
@@ -20,5 +21,18 @@ func InitApp() error {
 	librarian.Set("loket", func() (interface{}, error) {
 		return api.NewLoketApi("loket")
 	})
+	librarian.Set("mysql.master", func() (interface{}, error) {
+		return d.NewDatasourceFactory("mysqlaccess", "", c.GetString("mysql.master"))
+	})
+	librarian.Set("mysql.slave", func() (interface{}, error) {
+		return d.NewDatasourceFactory("mysqlaccess", "", c.GetString("mysql.slave"))
+	})
+	librarian.Set("redis.master", func() (interface{}, error) {
+		return cache.NewCacheStorageFactory("redis", c.GetString("redis.master"), "0")
+	})
+	librarian.Set("redis.slave", func() (interface{}, error) {
+		return cache.NewCacheStorageFactory("redis", c.GetString("redis.slave"), "0")
+	})
+
 	return nil
 }
